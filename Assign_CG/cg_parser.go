04f58a8dbbd995bc54101dd97a80ab0d800052cd
Prabhanshu Abhishek <pabhi@iitk.ac.in>
2016-01-31 19:01:17 +0530

    Added function to initialize 'instruction' struct, also added some more keywords in switch

--- Assign_CG/cg_parser.go
+++ Assign_CG/cg_parser.go
@@ -14,11 +14,21 @@ type Instr_struct struct {
 	Dest string
 	Src1 string
 	Src2 string
-	Jmp  bool
+	Jmp  string
 }
 
 var file string
 var instructions = make([]*Instr_struct, 0, 5)
+var leader = make(map[int]int)
+
+func initialize_instr(instr *Instr_struct, Op, Dest, Src1, Src2, Jmp string) {
+	instr.Op = Op
+	instr.Dest = Dest
+	instr.Src1 = Src1
+	instr.Src2 = Src2
+	instr.Jmp = Jmp
+	fmt.Println(instr, "parsed")
+}
 
 func parse_line(str string, line int) {
 	instr := new(Instr_struct)
@@ -29,14 +39,23 @@ func parse_line(str string, line int) {
 		log.Fatal("file line and instruction line no do not match")
 	}
 
+	if strconv.Itoa(line) != s[0] {
+		log.Fatal("Line mismatch, in sorce file and cg_parser")
+	}
+
 	switch s[1] {
 	case "+", "-", "*", "/":
-		instr.Op = s[1]
-		instr.Dest = s[2]
-		instr.Src1 = s[3]
-		instr.Src2 = s[4]
-		instr.Jmp = false
-		fmt.Println(instr, "case")
+		initialize_instr(instr, s[1], s[2], s[3], s[4], "0")
+	case "=":
+		initialize_instr(instr, s[1], s[2], s[3], "", "0")
+	case "ifgoto":
+		initialize_instr(instr, s[2], "", s[3], s[4], s[5])
+	case "call":
+		initialize_instr(instr, s[1], "", "", "", s[2])
+	case "ret":
+		initialize_instr(instr, s[1], "", "", "", "-1")
+	case "print":
+		initialize_instr(instr, s[1], "", s[2], "", "-2")
 	default:
 		fmt.Println(s[1], "hello")
 	}
